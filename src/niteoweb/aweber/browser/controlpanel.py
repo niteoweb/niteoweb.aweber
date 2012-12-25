3517dc801fa6efd52bb41fe8df19ca2be9527fb7
Matej Cotman <matejc@kiberpipa.org>
2012-12-25 19:59:48 +0100

    add code for schema.Choice

--- src/niteoweb/aweber/browser/controlpanel.py
+++ src/niteoweb/aweber/browser/controlpanel.py
@@ -7,6 +7,15 @@ Aweber control panel configlet
 from niteoweb.aweber import AweberMessageFactory as _
 from niteoweb.aweber.interfaces import IAweberSettings
 from plone.app.registry.browser import controlpanel
+from z3c.form import button
+from zope.schema.vocabulary import SimpleVocabulary
+
+
+def list_names():
+    terms = []
+    terms.append(SimpleVocabulary.createTerm(1, str(1), "one"))
+    terms.append(SimpleVocabulary.createTerm(2, str(2), "two"))
+    return SimpleVocabulary(terms)
 
 
 class AweberSettingsEditForm(controlpanel.RegistryEditForm):
@@ -15,6 +24,31 @@ class AweberSettingsEditForm(controlpanel.RegistryEditForm):
     schema = IAweberSettings
     label = _(u"Aweber settings")
     description = _(u"""Configure integration with Aweber API.""")
+
+    @button.buttonAndHandler(_('Get access'), name='get_access')
+    def handle_get_access_action(self, action):
+        super(AweberSettingsEditForm, self).updateWidgets()
+
+        print "##### CONSUMER INFO #####\nkey:{0}, secret:{1}".format(
+            self.widgets['consumer_key'].value,
+            self.widgets['consumer_key'].value
+        )
+
+        self.widgets['access_token'].value = unicode("token3253545445")
+        self.widgets['access_secret'].value = unicode("secret2453t30960")
+
+    @button.buttonAndHandler(_('Get list names'), name='get_lists')
+    def handle_get_lists_action(self, action):
+        self.widgets['list_name'].field.vocabulary = list_names()
+        super(AweberSettingsEditForm, self).updateWidgets()
+
+    @button.buttonAndHandler(_('Save'), name=None)
+    def handleSave(self, action):
+        super(AweberSettingsEditForm, self).handleSave(self, action)
+
+    @button.buttonAndHandler(_('Cancel'), name='cancel')
+    def handleCancel(self, action):
+        super(AweberSettingsEditForm, self).handleCancel(self, action)
 
 
 class AweberSettingsControlPanel(controlpanel.ControlPanelFormWrapper):
--- src/niteoweb/aweber/interfaces.py
+++ src/niteoweb/aweber/interfaces.py
@@ -1,5 +1,17 @@
-from zope.interface import Interface
+from five import grok
 from zope import schema
+from zope.interface import Interface
+from zope.schema.interfaces import IContextSourceBinder
+from zope.schema.vocabulary import SimpleVocabulary
+
+
+#@grok.provider(IContextSourceBinder)
+#def list_names(context):
+def list_names():
+    terms = []
+    terms.append(SimpleVocabulary.createTerm(1, str(1), "one"))
+    terms.append(SimpleVocabulary.createTerm(2, str(2), "two"))
+    return SimpleVocabulary(terms)
 
 
 class IAweberSettings(Interface):
@@ -7,12 +19,23 @@ class IAweberSettings(Interface):
     consumer_key = schema.TextLine(
         title=u"Consumer Key",
     )
+
     consumer_secret = schema.TextLine(
         title=u"Consumer Secret",
     )
+
     access_token = schema.TextLine(
         title=u"Access Token",
+        required=False,
     )
+
     access_secret = schema.TextLine(
         title=u"Access Secret",
+        required=False,
+    )
+
+    list_name = schema.Choice(
+        title=u"List Name",
+        values=["empty"],
+        required=False,
     )
