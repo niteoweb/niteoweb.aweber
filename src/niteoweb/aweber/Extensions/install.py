2f8622247eb209eba152d13a070cbe25d77eb78e
Matej Cotman <matejc@kiberpipa.org>
2012-12-25 19:51:22 +0100

    add uninstall code

---
+++ src/niteoweb/aweber/Extensions/install.py
@@ -0,0 +1,9 @@
+from Products.CMFCore.utils import getToolByName
+
+
+def uninstall(portal):
+    setup_tool = getToolByName(portal, 'portal_setup')
+    setup_tool.runAllImportStepsFromProfile(
+        'profile-niteoweb.aweber:uninstall'
+    )
+    return "Ran all uninstall steps."
