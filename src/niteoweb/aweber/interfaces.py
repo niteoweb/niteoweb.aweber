c75a5749e8643f52e5d71d7a325dd16b25ce4313
Matej Cotman <matejc@kiberpipa.org>
2013-01-10 15:13:58 +0100

    add vocabulary

--- src/niteoweb/aweber/interfaces.py
+++ src/niteoweb/aweber/interfaces.py
@@ -1,20 +1,42 @@
 from five import grok
 from zope import schema
 from zope.interface import Interface
-from zope.schema.interfaces import IContextSourceBinder
 from zope.schema.vocabulary import SimpleVocabulary
+from zope.schema.interfaces import IVocabularyFactory
+from plone import api
 
 
-#@grok.provider(IContextSourceBinder)
-#def list_names(context):
-def list_names():
-    terms = []
-    terms.append(SimpleVocabulary.createTerm(1, str(1), "one"))
-    terms.append(SimpleVocabulary.createTerm(2, str(2), "two"))
-    return SimpleVocabulary(terms)
+class ListsVocabulary(object):
+    grok.implements(IVocabularyFactory)
+
+    def __call__(self, context):
+        try:
+            lists_record = api.portal.get_registry_record(
+                'niteoweb.aweber.available_lists_record'
+            )
+            if lists_record:
+                return SimpleVocabulary.fromValues(lists_record)
+        except KeyError:
+            pass
+        return SimpleVocabulary.fromValues([])
+
+grok.global_utility(
+    ListsVocabulary,
+    name="niteoweb.aweber.available_lists_vocabulary",
+)
 
 
 class IAweberSettings(Interface):
+
+    app_id = schema.TextLine(
+        title=u"App ID",
+        required=False,
+    )
+
+    authorization_code = schema.TextLine(
+        title=u"Authorization Code",
+        required=False,
+    )
 
     consumer_key = schema.TextLine(
         title=u"Consumer Key",
@@ -26,16 +48,14 @@ class IAweberSettings(Interface):
 
     access_token = schema.TextLine(
         title=u"Access Token",
-        required=False,
     )
 
     access_secret = schema.TextLine(
         title=u"Access Secret",
-        required=False,
     )
 
     list_name = schema.Choice(
         title=u"List Name",
-        values=["empty"],
+        vocabulary="niteoweb.aweber.available_lists_vocabulary",
         required=False,
     )
