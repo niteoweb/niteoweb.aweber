f1b8829d8e4c16bd6d67fdcdf1b9c0362df9084d
Matej Cotman <matejc@kiberpipa.org>
2013-01-10 15:12:04 +0100

    add code for control panel

--- src/niteoweb/aweber/browser/controlpanel.py
+++ src/niteoweb/aweber/browser/controlpanel.py
@@ -8,14 +8,39 @@ from niteoweb.aweber import AweberMessageFactory as _
 from niteoweb.aweber.interfaces import IAweberSettings
 from plone.app.registry.browser import controlpanel
 from z3c.form import button
-from zope.schema.vocabulary import SimpleVocabulary
+from plone import api
+from aweber_api import AWeberAPI
 
 
-def list_names():
-    terms = []
-    terms.append(SimpleVocabulary.createTerm(1, str(1), "one"))
-    terms.append(SimpleVocabulary.createTerm(2, str(2), "two"))
-    return SimpleVocabulary(terms)
+def set_list_names(widgets):
+    """Get list names.
+    """
+    consumer_key = widgets['consumer_key'].value
+    consumer_secret = widgets['consumer_secret'].value
+    access_token = widgets['access_token'].value
+    access_secret = widgets['access_secret'].value
+
+    aweber = AWeberAPI(consumer_key, consumer_secret)
+    account = aweber.get_account(access_token, access_secret)
+    api.portal.set_registry_record(
+        'niteoweb.aweber.available_lists_record',
+        [l.name for l in account.lists]
+    )
+
+
+def parse_auth_code(widgets):
+    """Parse authorization code.
+    """
+    auth = AWeberAPI.parse_authorization_code(
+        widgets['authorization_code'].value
+    )
+
+    c_key, c_secret, a_key, a_secret = auth
+
+    widgets['consumer_key'].value = c_key
+    widgets['consumer_secret'].value = c_secret
+    widgets['access_token'].value = a_key
+    widgets['access_secret'].value = a_secret
 
 
 class AweberSettingsEditForm(controlpanel.RegistryEditForm):
@@ -23,24 +48,37 @@ class AweberSettingsEditForm(controlpanel.RegistryEditForm):
 
     schema = IAweberSettings
     label = _(u"Aweber settings")
-    description = _(u"""Configure integration with Aweber API.""")
+    description = _(u"""Configure integration with Aweber API.<br>
+    1. Enter App ID and click 'Get auth code'<br>
+    2. Follow the link on top of the page<br>
+    3. Enter your credentials
+    and copy over authentication code to second field<br>
+    4. Click second button to parse authentication code and update list names
+    """)
 
-    @button.buttonAndHandler(_('Get access'), name='get_access')
-    def handle_get_access_action(self, action):
-        super(AweberSettingsEditForm, self).updateWidgets()
-
-        print "##### CONSUMER INFO #####\nkey:{0}, secret:{1}".format(
-            self.widgets['consumer_key'].value,
-            self.widgets['consumer_key'].value
+    @button.buttonAndHandler(_('Get auth code'), name='get_auth')
+    def handle_get_auth_action(self, action):
+        app_id = self.widgets['app_id'].value
+        url = "https://auth.aweber.com/1.0/oauth/authorize_app/{0}".format(
+            app_id
+        )
+        api.portal.show_message(
+            message="Visit '{0}' and copy authorization code "
+                    "to Authorization Code field".format(url),
+            request=self.request
         )
 
-        self.widgets['access_token'].value = unicode("token3253545445")
-        self.widgets['access_secret'].value = unicode("secret2453t30960")
+    @button.buttonAndHandler(
+        _('Parse auth code and update lists'),
+        name='parse_auth'
+    )
+    def handle_parse_auth_action(self, action):
+        parse_auth_code(self.widgets)
+        set_list_names(self.widgets)
 
-    @button.buttonAndHandler(_('Get list names'), name='get_lists')
-    def handle_get_lists_action(self, action):
-        self.widgets['list_name'].field.vocabulary = list_names()
-        super(AweberSettingsEditForm, self).updateWidgets()
+    @button.buttonAndHandler(_('Update lists only'), name='update_lists')
+    def handle_update_lists_action(self, action):
+        set_list_names(self.widgets)
 
     @button.buttonAndHandler(_('Save'), name=None)
     def handleSave(self, action):
